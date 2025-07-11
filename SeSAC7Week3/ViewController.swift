e25b21d98f14f252ec4cb629d81416bb2fd014c1
ddudi <ddudios@naver.com>
2025-07-11 11:15:15 +0900

    print 2 삭제

--- SeSAC7Week3/ViewController.swift
+++ SeSAC7Week3/ViewController.swift
@@ -12,7 +12,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print(0)
-        print(2)
         print(3)
         print(4)
         print(5)
